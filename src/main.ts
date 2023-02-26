6aff92157d3bca92d0eedf218ad0b0835d05440d
fangliji <736419409@qq.com>
2023-02-26 21:41:50 +0800

    cors 解决跨域问题

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 // 全局中间件，通过app.use()引用
 // function MiddleWareAll(req: any, res: any, next: any) {
@@ -9,6 +10,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   // app.use(MiddleWareAll);
   await app.listen(3000);
   if (module.hot) {
